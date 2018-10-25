c9a833bdf4102e16588e71828b65cac08a63543f
Stwillia94 <stwillia@utica.edu>
2018-10-25 19:03:20 -0400

    BuzzBuzz

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -1,2 +1,67 @@
+
 public class FizzBuzz {
-}
+
+
+    public String testThree(int i) {
+        if (i % 3 == 0){
+            return "Fizz";
+        }
+        return String.valueOf(i);
+    }
+
+
+
+
+
+    public String testFive(int i) {
+        if ( i % 5 == 0){
+            return "Buzz";
+
+        }
+        return String.valueOf(i);
+
+    }
+
+
+
+
+    public String testThreeFive(int i) {
+        if (i % 15 == 0){
+            return "FizzBuzz";
+
+        }
+        return String.valueOf(i);
+
+    }
+
+
+
+
+
+    public String testNeither(int x) {
+        return String.valueOf(x);
+    }
+
+
+    public String fizzBuzzChecker(int x) {
+        if (x % 15 == 0) {
+            return "FizzBuzz";
+        }
+        else if (x % 3 == 0) {
+            return "Fizz";
+
+        }
+        else if (x % 5 == 0){
+            return "Buzz";
+        }
+        else {
+            return String.valueOf(x);
+        }
+    }
+
+    }
+
+
+
+
+    
--- src/test/java/FizzBuzzTest.java
+++ src/test/java/FizzBuzzTest.java
@@ -1,2 +1,58 @@
-public class FizzBuzzTest {
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class FizzBuzzTest
+{
+    FizzBuzz fizzBuzz;
+
+    @Before
+    public void setup(){
+        fizzBuzz = new FizzBuzz();
+    }
+
+    @Test
+    public void testThree(){
+        // Given
+        String expected = "Fizz";
+
+        //When
+        String actual = fizzBuzz.fizzBuzzChecker(3);
+
+        //Then
+        Assert.assertEquals(expected, actual);
+    }
+
+
+    @Test
+    public void testFive(){
+        // Given - setup
+
+        String actual = fizzBuzz.fizzBuzzChecker(5);
+
+        String expected = "Buzz";
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testThreeFive() {
+        // Given - setup
+
+        String actual = fizzBuzz.fizzBuzzChecker(15);
+
+        String expected = "FizzBuzz";
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testNeither() {
+        // Given - setup
+
+        String actual = fizzBuzz.fizzBuzzChecker(2);
+
+        String expected = "2";
+        Assert.assertEquals(expected, actual);
+    }
+
+
 }
